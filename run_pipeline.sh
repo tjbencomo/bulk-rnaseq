9d2c828635615ae95b8b7aa022b8de83cabbdc00
Tomas Bencomo <tjbencomo@gmail.com>
2020-12-06 15:58:35 -0800

    Move to the right directory

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=bulk-rnaseq
-#SBATCH --output=/scratch/users/tbencomo/logs/bulk-rnaseq.out
+#SBATCH --output=
 #SBATCH --nodes=1
 #SBATCH --time=01-00:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4000
 #SBATCH --mail-type=END
-#SBATCH --mail-user=tbencomo@stanford.edu
+#SBATCH --mail-user=
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
 set -e

025f8f9e51b8b4e509ced6bd768e6f96494f3131
Tomas Bencomo <tjbencomo@gmail.com>
2020-12-10 12:35:12 -0800

    Fix pipeline SLURM script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=bulk-rnaseq
-#SBATCH --output=
+#SBATCH --output=/scratch/users/tbencomo/logs/bulk-rnaseq.out
 #SBATCH --nodes=1
-#SBATCH --time=01-00:00:00
+#SBATCH --time=00-08:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4000
+#SBATCH --mem=2000
 #SBATCH --mail-type=END
-#SBATCH --mail-user=
+#SBATCH --mail-user=tbencomo@stanford.edu
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
 set -e
-cd $DIR
+cd $1
+echo "Starting up snakemake..."
 snakemake --cluster-config cluster.json -j 499 \
     --use-conda --use-singularity \
     --cluster 'sbatch -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} -c {cluster.ncpus} -o {cluster.out}'

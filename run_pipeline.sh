7f27817ffb008cbb6d197335340e2386e2260d8b
Tomas Bencomo <tjbencomo@gmail.com>
2021-03-31 23:36:57 -0700

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --time=00-04:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=2000
+#SBATCH --mem=8000
 #SBATCH --mail-type=END
 #SBATCH --mail-user=tbencomo@stanford.edu
 
